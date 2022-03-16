8e563943119f7168b38f780402b64abd9ff43b6c
Broccoli <63042276+HermitBroccoli@users.noreply.github.com>
2022-03-16 22:56:12 +1000

    finish first version project

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+new Splide('.splide', {
+    type: 'loop',
+    perPage: 1,
+    fixedWidth: "1071px",
+    gap: "10rem",
+    padding: { left: "5rem", right: "10rem" },
+    pagination: false,
+}).mount();
